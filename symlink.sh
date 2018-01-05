0ab1a0ffb419583176b1015b8b1819de16823ce5
Takuma Hashimoto <ma16079@shibaura-it.ac.jp>
2018-01-05 22:59:45 +0900

    グローバルの gitignore と gitconfig を設定

--- symlink.sh
+++ symlink.sh
@@ -12,3 +12,10 @@ ln -sf "$(pwd)/.tmux.conf" ~/.tmux.conf
 ln -sf "$(pwd)/dircolors.256dark" ~/.dircolors
 ln -sf "$(pwd)/.latexmkrc" ~/.latexmkrc
 ln -sf "$(pwd)/.key_list" ~/.key_list
+ln -sf "$(pwd)/.gitconfig.global" ~/.gitconfig
+if [[ -d ~/.config/git ]]; then
+  :
+else
+  mkdir -p ~/.config/git
+fi
+ln -sf "$(pwd)/.gitignore.global" ~/.config/git/ignore
