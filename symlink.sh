dac4993bb7299eba9d8cdb668759d1dcffadbd3b
Takuma Hashimoto <ma16079@shibaura-it.ac.jp>
2018-01-05 23:02:30 +0900

    s/$(pwd)/${PWD}/g

--- symlink.sh
+++ symlink.sh
@@ -4,18 +4,18 @@ set -eu
 
 DIR="$(dirname $0)"
 cd "${DIR}"
-ln -sf "$(pwd)/.zshrc" ~/.zshrc
-ln -sf "$(pwd)/.zshenv" ~/.zshenv
-ln -sf "$(pwd)/.vimrc" ~/.vimrc
-ln -sf "$(pwd)/.vim" ~/.vim
-ln -sf "$(pwd)/.tmux.conf" ~/.tmux.conf
-ln -sf "$(pwd)/dircolors.256dark" ~/.dircolors
-ln -sf "$(pwd)/.latexmkrc" ~/.latexmkrc
-ln -sf "$(pwd)/.key_list" ~/.key_list
-ln -sf "$(pwd)/.gitconfig.global" ~/.gitconfig
+ln -sf "${PWD}/.zshrc" ~/.zshrc
+ln -sf "${PWD}/.zshenv" ~/.zshenv
+ln -sf "${PWD}/.vimrc" ~/.vimrc
+ln -sf "${PWD}/.vim" ~/.vim
+ln -sf "${PWD}/.tmux.conf" ~/.tmux.conf
+ln -sf "${PWD}/dircolors.256dark" ~/.dircolors
+ln -sf "${PWD}/.latexmkrc" ~/.latexmkrc
+ln -sf "${PWD}/.key_list" ~/.key_list
+ln -sf "${PWD}/.gitconfig.global" ~/.gitconfig
 if [[ -d ~/.config/git ]]; then
   :
 else
   mkdir -p ~/.config/git
 fi
-ln -sf "$(pwd)/.gitignore.global" ~/.config/git/ignore
+ln -sf "${PWD}/.gitignore.global" ~/.config/git/ignore
